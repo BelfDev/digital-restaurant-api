81742d17473b694726485205d61dab2c30e04cd6
Pedro Belfort <belfortdev@gmail.com>
2020-07-30 17:53:38 +0100

    Add pg, pg-hstore, and sequelize dependencies

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { Sequelize } = require('sequelize');
 
 const dummyRouter = require('./routes/dummyRouter');
 

5dcd5ef5402762af0c3edb918a5bd6d219e14762
Pedro Belfort <belfortdev@gmail.com>
2020-08-04 22:03:09 +0100

    Introduce environment variables

---
+++ src/lib/env.js
@@ -0,0 +1,12 @@
+import yenv from 'yenv'
+import {logger} from './logger'
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'development'
+
+/**
+ * Utility script that returns values declared in the `env.yaml` file
+ */
+export const env = yenv('env.yaml', {
+    message: key => `[yenv] ${key} not found in the loaded environment`,
+    logBeforeThrow: message => logger.error(message)
+})

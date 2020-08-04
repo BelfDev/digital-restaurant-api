63317e9dc24ba4b8b39e461da5983dc9d81a6758
Pedro Belfort <belfortdev@gmail.com>
2020-08-04 22:04:47 +0100

    Create dependency injection container

---
+++ src/lib/container.js
@@ -0,0 +1,32 @@
+import {asValue, createContainer, InjectionMode, Lifetime} from 'awilix'
+import {logger} from './logger'
+
+/**
+ * Loads modules with Awilix dependency injection library.
+ */
+const modulesToLoad = [
+    // Each request gets a scoped (separate instance) of the service.
+    ['services/*.js', Lifetime.SCOPED],
+    // Stores are singletons; one per process.
+    ['stores/*.js', Lifetime.SINGLETON]
+]
+
+/**
+ * Configures a new container.
+ *
+ * @return {Object} The container.
+ */
+export function configureContainer() {
+    const opts = {
+        injectionMode: InjectionMode.CLASSIC
+    }
+    return createContainer(opts)
+        .loadModules(modulesToLoad, {
+            cwd: `${__dirname}/..`,
+            formatName: 'camelCase'
+        })
+        .register({
+            // Provide previously constructed logger as it is.
+            logger: asValue(logger)
+        })
+}

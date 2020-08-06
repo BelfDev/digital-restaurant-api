0510e963499d57f71f14b20778684fc0ea599d94
Pedro Belfort <belfortdev@gmail.com>
2020-08-06 17:51:57 +0100

    Add sequelize configuration files

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,23 +1,20 @@
-{
-  "development": {
-    "username": "root",
-    "password": null,
-    "database": "database_development",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  },
-  "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  }
+import {env} from '../../lib/env';
+
+const baseConfig = {
+    url: env.DB_URL,
+    dialect: 'postgres',
 }
+
+const config = {
+    development: {
+        ...baseConfig
+    },
+    test: {
+        ...baseConfig
+    },
+    production: {
+        ...baseConfig
+    },
+};
+
+export default config;

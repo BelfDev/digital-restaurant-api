eaeb2aaca2f95c50bbe38b39658c31f79de06bd6
Pedro Belfort <belfortdev@gmail.com>
2020-08-06 17:58:13 +0100

    Load all sequelize models

--- src/configs/database.js
+++
@@ -1,9 +0,0 @@
-const { Sequelize } = require('sequelize');
-
-/**
- * Sequelize object representing the postgres database connection.
- */
-export const db = new Sequelize('digital_restaurant', 'postgres', 'postgres', {
-    host: 'localhost',
-    dialect: 'postgres'
-});

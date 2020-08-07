01fc38928d6ecada4028839a98ac7bee7bd6d69f
Pedro Belfort <belfortdev@gmail.com>
2020-08-07 13:50:55 +0100

    Register cuisine and image association

--- src/database/config/associations.js
+++ src/database/config/associations.js
@@ -2,7 +2,8 @@ function withAssociations(sequelize) {
     const {Cuisines, Images} = sequelize.models;
 
     Cuisines.hasOne(Images, {
-        foreignKey: 'id'
+        foreignKey: 'id' ,
+        as: 'image'
     });
     Images.belongsTo(Cuisines, {
         foreignKey: 'id'

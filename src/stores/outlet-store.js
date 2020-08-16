bdf0bd0e91ac089af54099648eec563c8209e76c
Pedro Belfort <belfortdev@gmail.com>
2020-08-16 18:55:29 +0100

    Create Outlet store

---
+++ src/stores/outlet-store.js
@@ -0,0 +1,58 @@
+/**
+ * Data access layer for the Outlet model.
+ * Gets the logger and database injected.
+ * Methods: FIND, GET, CREATE, UPDATE, REMOVE.
+ */
+const createCuisineStore = (logger, db) => {
+    const Outlet = db.Outlets;
+    const options = {
+        include: [
+            {
+                model: db.Cuisines,
+                as: 'cuisine',
+                attributes: {
+                    exclude: ['id', 'imageId']
+                },
+            },
+            {
+                model: db.Locations,
+                as: 'location',
+                attributes: {
+                    exclude: ['id']
+                }
+            }
+        ],
+        attributes: {
+            exclude: [
+                'cuisineId',
+                'locationId',
+            ]
+        },
+    };
+
+    return {
+        /**
+         * Retrieves all outlet entities from the database.
+         * @returns {Promise<Outlets[]>}
+         */
+        async find() {
+            logger.debug('Retrieving outlets...')
+            return await Outlet.findAll(options);
+        },
+        /**
+         * Retrieve a specific cuisine entity from the database via its private key.
+         * @param id integer identifier matching the entity Primary Key
+         * @returns {Promise<Outlets>}
+         */
+        async get(id) {
+            logger.debug(`Getting outlet with id ${id}`)
+            const foundOutlet = await Outlet.findByPk(id, options);
+            if (!foundOutlet) {
+                return null
+            }
+            return foundOutlet;
+        },
+    };
+};
+
+export default createCuisineStore;

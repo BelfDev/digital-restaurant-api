f377ef745abba10ae33ad9aa81cb8c93ab0217ec
Pedro Belfort <belfortdev@gmail.com>
2020-08-16 17:04:33 +0100

    Define Outlet and OutletImage relationships

--- src/database/config/associations.js
+++ src/database/config/associations.js
@@ -1,13 +1,47 @@
 function withAssociations(sequelize) {
-    const {Cuisines, Images} = sequelize.models;
+    const {Cuisines, Images, Outlets, Locations, OutletImages} = sequelize.models;
 
+    // Cuisine relationship
     Cuisines.hasOne(Images, {
-        foreignKey: 'id' ,
+        foreignKey: 'id',
         as: 'image'
     });
     Images.belongsTo(Cuisines, {
         foreignKey: 'id'
     });
+
+    // Outlet relationship
+    Outlets.hasOne(Cuisines, {
+        foreignKey: 'id',
+        as: 'cuisine'
+    });
+    Cuisines.belongsTo(Outlets, {
+        foreignKey: 'id'
+    });
+    Outlets.hasOne(Locations, {
+        foreignKey: 'id',
+        as: 'location'
+    });
+    Locations.belongsTo(Outlets, {
+        foreignKey: 'id'
+    });
+
+    // Outlet-image relationship
+    OutletImages.hasOne(Outlets, {
+        foreignKey: 'id',
+        as: 'outlet'
+    });
+    OutletImages.hasOne(Images, {
+        foreignKey: 'id',
+        as: 'image'
+    });
+    Outlets.belongsTo(OutletImages, {
+        foreignKey: 'id',
+    });
+    Images.belongsTo(OutletImages, {
+        foreignKey: 'id',
+    });
+
 }
 
 module.exports = {withAssociations};

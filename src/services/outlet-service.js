16244b8d78d00ec30d158d2c91ae8ba7de814652
Pedro Belfort <belfortdev@gmail.com>
2020-08-17 16:53:55 +0100

    Create the outlet service

--- src/services/outlet-service.js
+++ src/services/outlet-service.js
@@ -8,14 +8,15 @@ const assertId = BadRequest.makeAssert('No id given')
  * Gets an outlet store injected.
  */
 export default class OutletService {
-    constructor(outletStore) {
+    constructor(outletStore, featuredStore) {
         this.outletStore = outletStore;
+        this.featuredStore = featuredStore;
     }
 
     /**
      * Fetch all outlets according to the given parameter.
      * @param params optional parameters to filter the result.
-     * @returns {Promise<*>}
+     * @returns {Promise<{result: Outlets[]}>}
      */
     async find(params) {
         const result = await this.outletStore.find(params);
@@ -23,9 +24,19 @@ export default class OutletService {
     }
 
     /**
+     * Fetch all featured outlets according to the given parameter.
+     * @param params optional parameters to filter the result.
+     * @returns {Promise<{result: Outlets[]}>}
+     */
+    async findFeatured(params) {
+        const result = await this.featuredStore.find(params);
+        return {result};
+    }
+
+    /**
      * Searches for a specific outlet by the given identifier.
      * @param id integer identifier
-     * @returns {Promise<*>}
+     * @returns {Promise<{result: Outlets}>}
      */
     async get(id) {
         assertId(id)
@@ -34,4 +45,5 @@ export default class OutletService {
         );
         return {result};
     }
+
 }

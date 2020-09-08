8019d5f3320cf566e9a7f96942b7ed73deb5ce1c
Pedro Belfort <belfortdev@gmail.com>
2020-09-07 22:30:10 -0300

    Create addCarts method in outlet service

--- src/services/order-service.js
+++ src/services/order-service.js
@@ -2,6 +2,8 @@ import {BadRequest, NotFound} from 'fejl'
 
 // Prefab assert function.
 const assertId = BadRequest.makeAssert('No id given')
+const assertSessionId = BadRequest.makeAssert('No sessionId given')
+const assertCartId = BadRequest.makeAssert('No cartId given')
 
 /**
  * Order service layer.
@@ -21,7 +23,9 @@ export default class OrderService {
         const result = await this.orderStore.find();
         return {result};
     }
+
     ø
+
     /**
      * Searches for a specific order by the given identifier.
      * @param id integer identifier
@@ -34,4 +38,66 @@ export default class OrderService {
         );
         return {result};
     }
+
+    /**
+     * Adds carts to an order
+     * sessionId and carts must be passed
+     * if orderId is omitted from the query parameters, a new order is created
+     */
+    async addCarts(ctx) {
+        const sessionId = ctx.session.id;
+        const body = ctx.request.body;
+
+        let orderId = ctx.params.id;
+
+        assertSessionId(sessionId);
+
+        if (!orderId) {
+            const order = await this.orderStore.create();
+            orderId = order.id;
+        }
+
+        if (body && body['carts']) {
+            const data = body['carts'].map((item) => {
+                return {
+                    cartId: item.id,
+                    orderId: orderId,
+                }
+            });
+
+            await this.orderStore.bulkAssociateCarts(data);
+
+            const order = await this.orderStore.get(orderId);
+
+            const result = this._formatOrderObject(order);
+
+            return {result};
+        }
+
+        return ctx.throw(400);
+    }
+
+    _formatOrderObject(order) {
+        let result = order;
+        if (order['carts']) {
+            result.carts = order.carts.map((cart) => this._formatCartObject(cart))
+        }
+        return result;
+    }
+
+    _formatCartObject(cart) {
+        let result = cart.dataValues;
+        if (cart['items']) {
+            result.items = cart.items.map((item) => {
+                const newItem = item.dataValues;
+                delete newItem['cartItem'];
+                return {
+                    ...newItem,
+                    ...item['cartItem'].dataValues
+                }
+            })
+        }
+        return result;
+    }
+
 }

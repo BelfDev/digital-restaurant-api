077c7ec489e0d7c9c982c30ea84d87776cdd6de2
Pedro Belfort <belfortdev@gmail.com>
2020-09-07 22:29:35 -0300

    Add order-api endpoints

--- src/routes/order-api.js
+++ src/routes/order-api.js
@@ -1,4 +1,5 @@
 import {createController} from 'awilix-koa'
+import bodyParser from "koa-bodyparser";
 
 /**
  * Cuisine API controller abstraction.
@@ -7,6 +8,9 @@ import {createController} from 'awilix-koa'
 const api = orderService => ({
     findOrders: async ctx => ctx.ok(await orderService.find(ctx.query)),
     getOrder: async ctx => ctx.ok(await orderService.get(ctx.params.id)),
+    createOrder: async ctx => ctx.ok(await orderService.createOrder(ctx)),
+    updateOrder: async ctx => ctx.ok(await orderService.updateOrder(ctx)),
+    addCarts: async ctx => ctx.ok(await orderService.addCarts(ctx)),
 });
 
 /**
@@ -15,4 +19,20 @@ const api = orderService => ({
 export default createController(api)
     .prefix('/orders')
     .get('', 'findOrders')
-    .get('/:id', 'getOrder');
+    .get('/:id', 'getOrder')
+    .post('/', 'createOrder', {
+        // Maps `POST /todos` to the `createTodo` function on the returned object from `API`
+        before: [bodyParser()] // Runs the bodyParser just for this endpoint
+    })
+    .patch('/:id', 'updateOrder', {
+        // Maps `POST /todos` to the `createTodo` function on the returned object from `API`
+        before: [bodyParser()] // Runs the bodyParser just for this endpoint
+    })
+    .post('/carts', 'addCarts', {
+        // Maps `POST /todos` to the `createTodo` function on the returned object from `API`
+        before: [bodyParser()] // Runs the bodyParser just for this endpoint
+    })
+    .post('/:id/carts', 'addCarts', {
+        // Maps `POST /todos` to the `createTodo` function on the returned object from `API`
+        before: [bodyParser()] // Runs the bodyParser just for this endpoint
+    });
